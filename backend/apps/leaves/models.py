9607439f52e286fe605ab4334667d84abdbb4f36
AbhinandVJ6 <abhinand@techis.io>
2022-12-11 12:06:11 +0530

    created-tables-for-leaves-app-and-created-constants-file-in-project-with-leave-choices

--- backend/apps/leaves/models.py
+++ backend/apps/leaves/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from apps.users.models import User
+from config.constants import LEAVE_TYPE
 
 # Create your models here.
+class Leave(models.Model):
+    class meta(object):
+        db_table = 'leave'
+
+
+    employee_id = models.ForeignKey(User, blank=False, null= False, db_index=True)
+    user_name = models.ForeignKey(User, blank=False, null=False, db_index=True)
+    leave_type = models.CharField('Leave Type', blank=False, null=False, default='N/A', choices=LEAVE_TYPE, max_length=50)
+    from_date = models.DateTimeField('From Date', blank=False, null=False)
+    to_date = models.DateTimeField('To Date', blank=False, null=False)
+    duration = models.IntegerField('Duration', blank=False, null=False,default=0)
+    created_at = models.DateTimeField('Created Datetime', blank=False, auto_now_add=True)
+    updated_at = models.DateTimeField('Updated Datetime', blank=False, auto_now=True)
+
+    def __str__(self):
+        return self.name
+
---
+++ backend/config/constants.py
@@ -0,0 +1,8 @@
+
+
+LEAVE_TYPE = (
+    ('casual_leave', 'Casual Leave'),
+    ('sick_leave', 'Sick Leave'),
+    ('maternity_leave', 'Maternity Leave'),
+    ('paternity_leave', 'Paternity Leave'),
+)
