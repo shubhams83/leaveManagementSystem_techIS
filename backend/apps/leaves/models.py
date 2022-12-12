69b76b81a846657b1e8cd0876251e0327555537d
AbhinandVJ6 <abhinand@techis.io>
2022-12-12 08:02:04 +0530

    added all table data and choice fields for leave status and leave type added two leaves from backend

--- backend/apps/leaves/models.py
+++ backend/apps/leaves/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
-from apps.users.models import User
-from config.constants import LEAVE_TYPE
+# from apps.users.models import User
+from config.constants import LEAVE_TYPE , LEAVE_STATUS
 
 # Create your models here.
 class Leave(models.Model):
@@ -11,14 +11,15 @@ class Leave(models.Model):
         db_table = 'leave'
 
 
-    employee_id = models.CharField('Emp.Ids:', blank=False, null= False, db_index=True)
-    user_name = models.CharField('Name', blank=False, null=False, db_index=True)
+    employee_id = models.IntegerField('Emp.Id:', blank=False, null= False,default=0)
+    user_name = models.CharField('Name', blank=False, null=False, db_index=True,max_length=40)
     leave_type = models.CharField('Leave Type', blank=False, null=False, default='N/A', choices=LEAVE_TYPE, max_length=50)
     from_date = models.DateTimeField('From Date', blank=False, null=False)
     to_date = models.DateTimeField('To Date', blank=False, null=False)
     duration = models.IntegerField('Duration', blank=False, null=False,default=0)
     created_at = models.DateTimeField('Created Datetime', blank=False, auto_now_add=True)
     updated_at = models.DateTimeField('Updated Datetime', blank=False, auto_now=True)
+    leave_status = models.CharField('Leave Status', blank=False, null=False, default='N/A', choices=LEAVE_STATUS, max_length=30)
 
     def __str__(self):
         return self.user_name
--- backend/config/constants.py
+++ backend/config/constants.py
@@ -1,8 +1,20 @@
 
 
 LEAVE_TYPE = (
+    ('n/a','N/A'),
     ('casual_leave', 'Casual Leave'),
     ('sick_leave', 'Sick Leave'),
     ('maternity_leave', 'Maternity Leave'),
     ('paternity_leave', 'Paternity Leave'),
+    ('loss_of_pay_leave','Loss of Pay Leave'),
+    ('bereavement_leave','Bereavement Leave'),
+)
+
+LEAVE_STATUS = (
+    ('n/a','N/A'),
+    ('approved','Approved'),
+    ('rejected','Rejected'),
+    ('pending','Pending'),
+    ('cancelled','Cancelled'),
+
 )
